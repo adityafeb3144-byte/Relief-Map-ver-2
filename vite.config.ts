e45763e67152bd4d7c5ed86981e069f318e3dd6b
adityafeb3144-byte <adityafeb3144@gmail.com>
2026-04-10 19:25:40 +0530

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Relief-Map-ver-2/', // ADD THIS LINE
+  base: '/Relief-Map-ver-2/', // THIS MUST MATCH YOUR REPO NAME EXACTLY
 })
